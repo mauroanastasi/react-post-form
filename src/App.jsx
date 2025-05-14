64c1732e44d24f9bcad8a07b5eeae74a847f4bfc
mauroanastasi <mauroanastasi1@gmail.com>
2025-05-15 01:40:49 +0200

    [UPD] vado a richiamare la funzione appena creata tramite onChange su ogni campo input

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,18 @@ function App() {
       <form className='border rounded bg-light p-4'>
         <div className="mb-3">
           <label for="exsampleAuthor" className="form-label">Autore del post</label>
-          <input type="text" name="name" value={formData.name} className="form-control" id="exsampleAuthor" placeholder="inserisci l'autore" />
+          <input onChange={handleChange} type="text" name="name" value={formData.name} className="form-control" id="exsampleAuthor" placeholder="inserisci l'autore" />
         </div>
         <div className="mb-3">
           <label for="exsampleTitle" className="form-label">Titolo del post</label>
-          <input type="text" name="title" value={formData.title} className="form-control" id="exsampleTitle" placeholder="inserisci il titolo del post" />
+          <input onChange={handleChange} type="text" name="title" value={formData.title} className="form-control" id="exsampleTitle" placeholder="inserisci il titolo del post" />
         </div>
         <div className="mb-3">
           <label for="exampleFormControlTextarea1" className="form-label">Testo del post</label>
-          <textarea className="form-control" name="text" value={formData.text} id="exampleFormControlTextarea1" rows="3"></textarea>
+          <textarea onChange={handleChange} className="form-control" name="text" value={formData.text} id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
         <div className="form-check">
-          <input className="form-check-input" name="updates" checked={formData.updates} type="checkbox" id="checkIndeterminate" />
+          <input onChange={handleChange} className="form-check-input" name="updates" checked={formData.updates} type="checkbox" id="checkIndeterminate" />
           <label className="form-check-label" for="checkIndeterminate">
             Spunta la casella se vuoi che il post venga pubblicato, in caso contrario lo salveremo tra le tue bozze.
           </label>
