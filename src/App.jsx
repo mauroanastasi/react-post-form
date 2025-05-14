7918ecb743e759c8c99ec70f68a1cf3c9e3a7075
mauroanastasi <mauroanastasi1@gmail.com>
2025-05-15 01:30:19 +0200

    [UPD] creo variabile di stato che conterrà oggetto con i valori, inserisco name in ogni input

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    name: "",
+    title: "",
+    text: "",
+    updates: false
+  });
 
   return (
     <>
-      <div className="mb-3">
-        <label for="exsampleAuthor" className="form-label">Autore del post</label>
-        <input type="text" className="form-control" id="exsampleAuthor" placeholder="inserisci l'autore" />
-      </div>
-      <div className="mb-3">
-        <label for="exsampleTitle" className="form-label">Titolo del post</label>
-        <input type="text" className="form-control" id="exsampleTitle" placeholder="inserisci l'autore" />
-      </div>
-      <div className="mb-3">
-        <label for="exampleFormControlTextarea1" className="form-label">Testo del post</label>
-        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-      </div>
-      <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="checkIndeterminate" />
-        <label className="form-check-label" for="checkIndeterminate">
-          Spunta la casella se vuoi che il post venga pubblicato, in caso contrario lo salveremo tra le tue bozze.
-        </label>
-      </div>
+      <form className='border rounded bg-light p-4'>
+        <div className="mb-3">
+          <label for="exsampleAuthor" className="form-label">Autore del post</label>
+          <input type="text" name="name" className="form-control" id="exsampleAuthor" placeholder="inserisci l'autore" />
+        </div>
+        <div className="mb-3">
+          <label for="exsampleTitle" className="form-label">Titolo del post</label>
+          <input type="text" name="title" className="form-control" id="exsampleTitle" placeholder="inserisci il titolo del post" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlTextarea1" className="form-label">Testo del post</label>
+          <textarea className="form-control" name="text" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <div className="form-check">
+          <input className="form-check-input" name="updates" type="checkbox" value="" id="checkIndeterminate" />
+          <label className="form-check-label" for="checkIndeterminate">
+            Spunta la casella se vuoi che il post venga pubblicato, in caso contrario lo salveremo tra le tue bozze.
+          </label>
+        </div>
+        <div className='m-3'>
+          <button type='submit' className='btn btn-primary' placeholder="invia dati">invia dati</button>
+        </div>
+      </form>
     </>
   )
 }
